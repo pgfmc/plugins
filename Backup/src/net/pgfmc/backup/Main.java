55f3b5931162d0a184a3e6ac2cfb011456c56516
bk <bk123yt@gmail.com>
2022-01-16 18:54:56 -0600

    Backup Optimizations

--- Backup/src/net/pgfmc/backup/Main.java
+++ Backup/src/net/pgfmc/backup/Main.java
@@ -1,7 +1,10 @@
 package net.pgfmc.backup;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.coreprotect.CoreProtect;
+import net.coreprotect.CoreProtectAPI;
 import net.pgfmc.backup.backup.Backup;
 import net.pgfmc.backup.backup.Backupconfirm;
 import net.pgfmc.backup.backup.Restore;
@@ -26,6 +29,28 @@ public class Main extends JavaPlugin {
 	public void onDisable()
 	{
 		
+	}
+	
+	public CoreProtectAPI getCoreProtect() {
+        Plugin p = getServer().getPluginManager().getPlugin("CoreProtect");
+
+        // Check that CoreProtect is loaded
+        if (p == null || !(p instanceof CoreProtect)) {
+            return null;
+        }
+
+        // Check that the API is enabled
+        CoreProtectAPI CoreProtect = ((CoreProtect) p).getAPI();
+        if (CoreProtect.isEnabled() == false) {
+            return null;
+        }
+
+        // Check that a compatible version of the API is loaded
+        if (CoreProtect.APIVersion() < 7) {
+            return null;
+        }
+
+        return CoreProtect;
 	}
 
 }
