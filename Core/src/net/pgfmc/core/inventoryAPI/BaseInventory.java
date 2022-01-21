6109c5a275929a74671af34b5abda697b35718df
CrimsonDart <79547580+CrimsonDart@users.noreply.github.com>
2022-01-21 02:23:55 -0600

    small update, makes this work more efficiently

--- Core/src/net/pgfmc/core/inventoryAPI/BaseInventory.java
+++ Core/src/net/pgfmc/core/inventoryAPI/BaseInventory.java
@@ -1,7 +1,5 @@
 package net.pgfmc.core.inventoryAPI;
 
-import java.util.ArrayList;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -9,8 +7,8 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
+import net.pgfmc.core.CoreMain;
 import net.pgfmc.core.inventoryAPI.extra.Butto;
 import net.pgfmc.core.inventoryAPI.extra.ItemWrapper;
 import net.pgfmc.core.inventoryAPI.extra.SizeData;
@@ -57,92 +55,23 @@ public class BaseInventory implements InventoryHolder {
 	public ItemWrapper setItem(int slot, ItemStack itemS) {
 		if (slot + 1 > inv.getSize()) return null;
 		
-		ItemWrapper iw = new ItemWrapper(itemS) {
-			@Override
-			public ItemWrapper a(int a) {
-				item.setAmount(a);
-				inv.setItem(slot, item);
-				return this;
-			}
-			
-			@Override
-			public ItemWrapper n(String name) {
-				ItemMeta imeta = item.getItemMeta();
-				imeta.setDisplayName(name);
-				item.setItemMeta(imeta);
-				inv.setItem(slot, item);
-				return this;
-			}
-			
-			@Override
-			public ItemWrapper l(String lore) {
-				ItemMeta imeta = item.getItemMeta();
-				
-				String[] lorelist = lore.split("\n");
-				ArrayList<String> liszt = new ArrayList<>(lorelist.length);
-				for (String s : lorelist) {
-					liszt.add(s);
-				}
-				imeta.setLore(liszt);
-				item.setItemMeta(imeta);
-				inv.setItem(slot, item);
-				return this;
-			}
-			
-			@Override
-			public ItemWrapper m(Material mat) {
-				item.setType(mat);
-				inv.setItem(slot, item);
-				return this;
-			}
-		};
-		inv.setItem(slot, iw.gi());
+		ItemWrapper iw = new ItemWrapper(itemS);
+		
+		Bukkit.getScheduler().runTaskLater(CoreMain.plugin, x -> {
+			inv.setItem(slot, iw.gi());
+		}, 0);
+		
 		return iw;
 	}
 	
 	public ItemWrapper setItem(int slot, Material mat) {
 		if (slot + 1 > inv.getSize()) return null;
 		
-		ItemWrapper iw = new ItemWrapper(mat) {
-			@Override
-			public ItemWrapper a(int a) {
-				item.setAmount(a);
-				inv.setItem(slot, item);
-				return this;
-			}
-			
-			@Override
-			public ItemWrapper n(String name) {
-				ItemMeta imeta = item.getItemMeta();
-				imeta.setDisplayName(name);
-				item.setItemMeta(imeta);
-				inv.setItem(slot, item);
-				return this;
-			}
-			
-			@Override
-			public ItemWrapper l(String lore) {
-				ItemMeta imeta = item.getItemMeta();
-				
-				String[] lorelist = lore.split("\n");
-				ArrayList<String> liszt = new ArrayList<>(lorelist.length);
-				for (String s : lorelist) {
-					liszt.add(s);
-				}
-				imeta.setLore(liszt);
-				item.setItemMeta(imeta);
-				inv.setItem(slot, item);
-				return this;
-			}
-			
-			@Override
-			public ItemWrapper m(Material mat) {
-				item.setType(mat);
-				inv.setItem(slot, item);
-				return this;
-			}
-		};
-		inv.setItem(slot, iw.gi());
+		ItemWrapper iw = new ItemWrapper(mat);
+		Bukkit.getScheduler().runTaskLater(CoreMain.plugin, x -> {
+			inv.setItem(slot, iw.gi());
+		}, 0);
+		
 		return iw;
 	}
 	
