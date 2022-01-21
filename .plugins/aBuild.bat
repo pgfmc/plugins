0fad83605dc570a38fdc889c3962faf20d69eaac
FlamingClappz <80445075+FlamingClappz@users.noreply.github.com>
2022-01-20 22:35:51 -0500

    List Command
    Im PRAYING this works

--- .plugins/aBuild.bat
+++ .plugins/aBuild.bat
@@ -3,3 +3,4 @@ ECHO exporting jars..
 
 FOR %%n in (Backup, Bot, Core, Market, Masterbook, ModTools, Survival, Teleport) DO jar cfv %%n.jar -C E:\github\%%n\bin .
 jar cfv Teams.jar -C E:\github\teams\bin .
+PAUSE
