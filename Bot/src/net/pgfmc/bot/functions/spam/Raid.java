342726d656ca949b8f8c49ae38f7519416332e87
bk <bk123yt@gmail.com>
2022-01-29 14:16:54 -0600

    Antispam + other

--- Bot/src/net/pgfmc/bot/functions/spam/Raid.java
+++ Bot/src/net/pgfmc/bot/functions/spam/Raid.java
@@ -1,5 +1,60 @@
 package net.pgfmc.bot.functions.spam;
 
+import java.time.OffsetDateTime;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.pgfmc.bot.Discord;
+
 public class Raid {
+	
+	private static LinkedList<String> last5MemberIds = new LinkedList<>();
+	
+	private static long lastJoin = 0L;
+	private static double joinPerSecond = 0;
+
+	public static void check(GuildMemberJoinEvent e) {
+		Guild g = e.getGuild();
+		
+		if (g.getId() != Discord.PGF_ID) return;
+		
+		double lastJoinInSeconds = (double) ((new Date().getTime() - lastJoin) / 1000);
+		double joins = joinPerSecond * lastJoinInSeconds + 1;
+		
+		joinPerSecond = joins / lastJoin;
+		lastJoin = new Date().getTime();
+		
+		if (last5MemberIds.size() > 5)
+		{
+			last5MemberIds.remove(0);
+		}
+		
+		last5MemberIds.add(e.getMember().getId());
+		
+		if (joinPerSecond >= 5)
+		{
+			Role muteRole = g.getRoleById("895443829031862342");
+			for (String id : last5MemberIds)
+			{
+				g.addRoleToMember(id, muteRole);
+			}
+			
+			EmbedBuilder eb = Discord.simpleServerEmbed("Raid detected!", "https://cdn.discordapp.com/emojis/726548471015800912.webp?size=96", Discord.red);
+			eb.setDescription("Last 5 members: " + last5MemberIds.stream()
+													.map(id -> g.getMemberById(id).getAsMention())
+													.collect(Collectors.toList())
+					);
+			eb.setTimestamp(OffsetDateTime.now());
+			
+			Discord.sendAlert(eb.build());
+			
+		}
+		
+	}
 
 }
--- Bot/src/net/pgfmc/bot/functions/spam/Spam.java
+++ Bot/src/net/pgfmc/bot/functions/spam/Spam.java
@@ -1,5 +1,57 @@
 package net.pgfmc.bot.functions.spam;
 
-public class Spam implements Punishable {
+import java.util.Date;
+import java.util.Optional;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+import net.pgfmc.bot.Discord;
+import net.pgfmc.bot.Main;
+import net.pgfmc.core.permissions.Permissions;
+import net.pgfmc.core.playerdataAPI.PlayerData;
+import net.pgfmc.core.punish.Punish;
+
+public class Spam {
+	
+	public static void check(Player p)
+	{
+		PlayerData pd = PlayerData.getPlayerData(p);
+		
+		// current time - last chat time <= 850ms, is not muted, does not have exemption
+		if (!Punish.isMute(pd) && !Permissions.has(pd.getPlayer(), "pgf.spam.exempt") && new Date().getTime() - (long) Optional.ofNullable(pd.getData("spam.lastchat")).orElse(1000L) <= 550)
+		{
+			byte warnings = (byte) Optional.ofNullable(pd.getData("spam.warnings")).orElse((byte) 1);
+			pd.setData("spam.warnings", (byte) (warnings + (byte) 1));
+			
+			if (warnings <= 3)
+			{
+				pd.sendMessage(ChatColor.RED + "Please, slow down (" + warnings + "/3)!");
+			}
+			
+			if (warnings >= 3 && !Permissions.has(pd.getPlayer(), "pgf.spam.exempt"))
+			{
+				pd.sendMessage(ChatColor.DARK_RED + "You've been muted for spamming (60 seconds).");
+				Discord.sendAlert(Discord.simplePlayerEmbed(pd.getPlayer(), "has been muted for spamming.", Discord.red).build());
+				Punish.setMute(pd, true);
+				addTimer(pd);
+			}
+			
+		}
+		
+		pd.setData("spam.lastchat", new Date().getTime());
+	}
+	
+	private static void addTimer(PlayerData pd)
+	{
+		Bukkit.getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable() {
+			public void run() {
+				Discord.sendAlert(Discord.simplePlayerEmbed(pd.getPlayer(), "has been unmuted.", Discord.green).build());
+				Punish.setMute(pd, false).sendMessage(ChatColor.GREEN + "You've been unmuted.");;
+				pd.setData("spam.warnings", (byte) 0);
+			}
+		}, 60 * 20);
+	}
+	
 }
