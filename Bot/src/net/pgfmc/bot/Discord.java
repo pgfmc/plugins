695187fc9a782b121d7d13bf47e0478738c018cd
bk <bk123yt@gmail.com>
2022-02-03 00:56:14 -0600

    Attempt fix #35, Fixed Configify empty constructor, cleaned up Bot

--- Bot/src/net/pgfmc/bot/Discord.java
+++ Bot/src/net/pgfmc/bot/Discord.java
@@ -1,8 +1,6 @@
 package net.pgfmc.bot;
 
 import java.awt.Color;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,6 +11,7 @@ import org.bukkit.OfflinePlayer;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -24,28 +23,23 @@ import net.pgfmc.bot.listeners.OnMessageReceived;
 import net.pgfmc.bot.listeners.OnReady;
 import net.pgfmc.bot.listeners.OnSlashCommand;
 import net.pgfmc.bot.listeners.OnUpdateRole;
+import net.pgfmc.core.CoreMain;
 
 public class Discord extends ListenerAdapter {
 	
 	public static JDA JDA = null;
-	public static String PREFIX = "!";
-	public static List<String> ADMINS = new ArrayList<>(Arrays.asList("243499063838769152", "440726027405361152")); // bk, Crimson
-	private static TextChannel serverChannel;
+	public static final String PREFIX = "!";
+	//public static List<String> ADMINS = new ArrayList<>(Arrays.asList("243499063838769152", "440726027405361152")); // bk, Crimson
+	//private static TextChannel serverChannel;
 	
-	public static String PGF_ID = "579055447437475851";
-	public static String BTS_ID = "721951670132801596";
+	//public static String PGF_ID = "579055447437475851";
+	//public static String BTS_ID = "721951670132801596";
 	
 	// Frequently used colors
-	public static final Color green = new Color(0, 255, 0); // Join
-	public static final Color red = new Color(255, 0, 0); // Leave
-	public static final Color gold = new Color(255, 215, 0); // Advancement
-	public static final Color black = new Color(0, 0, 0); // Death
-	
-	public static String SERVER_CHANNEL;
-	public static final String START_MESSAGE = "<:START:905682398790959125> Server has started!";
-	public static final MessageEmbed START_MESSAGE_EMBED = simpleServerEmbed("Server is starting...", "https://cdn.discordapp.com/emojis/905682398790959125.png?size=44", green).build();
-	public static final String STOP_MESSAGE = "<:STOP:905683316844429312> Server is stopping...";
-	public static final MessageEmbed STOP_MESSAGE_EMBED = simpleServerEmbed("Server is stopping...", "https://cdn.discordapp.com/emojis/905683316844429312.png?size=44", red).build();
+	public static final Color GREEN = new Color(0, 255, 0); // Join
+	public static final Color RED = new Color(255, 0, 0); // Leave
+	public static final Color GOLD = new Color(255, 215, 0); // Advancement
+	public static final Color BLACK = new Color(0, 0, 0); // Death
 	
 	public static void initialize() throws LoginException, InterruptedException {
 		JDABuilder builder = JDABuilder.createDefault(Secret.getKey()); // bot token, don't share.
@@ -74,40 +68,40 @@ public class Discord extends ListenerAdapter {
 	}
 	
 	public static void sendMessage(String m) {
-		if (m == null || m == "") {
-			return;
-		}
-		serverChannel.sendMessage(m).queue();
+		if (m == null || m == "") return;
+		
+		getServerChannel().sendMessage(m).queue();
 		System.out.println("Discord: " + m);
+	}
+	
+	public static void sendEmbed(MessageEmbed eb)
+	{
+		getServerChannel().sendMessage(eb).queue();
+		// System.out.println("Discord: " + eb.getAuthor().getName());
 	}
 	
 	public static void sendAlert(String m) {
 		if (m == null || m.equals("")) { return; }
-		JDA.getTextChannelById("891939656969621534").sendMessage(m).queue();
+		getAlertChannel().sendMessage(m).queue();
 	}
+	
 	public static void sendAlert(MessageEmbed me) {
 		if (me == null) { return; }
-		JDA.getTextChannelById("891939656969621534").sendMessage(me).queue();
+		getAlertChannel().sendMessage(me).queue();
 	}
 	
-	public static void setChannel(TextChannel server) {
-		if (serverChannel == null) {
-			serverChannel = server;
-		}
+	public static TextChannel getServerChannel() {
+		return JDA.getTextChannelById(CoreMain.machine.getServerChannelId());
 	}
 	
-	public static TextChannel getChannel() {
-		return serverChannel;
+	public static TextChannel getAlertChannel() {
+		return JDA.getTextChannelById(CoreMain.machine.getAlertChannelId());
 	}
 	
-	
-	
-	public static void sendEmbed(MessageEmbed eb)
+	public static Guild getGuildPGF()
 	{
-		serverChannel.sendMessage(eb).queue();
-		// System.out.println("Discord: " + eb.getAuthor().getName());
+		return JDA.getGuildById("579055447437475851");
 	}
-	
 	
 	
 	/**
@@ -118,13 +112,13 @@ public class Discord extends ListenerAdapter {
 	 * @param color The color of the embed
 	 * @return the EmbedBuilder
 	 */
-	public static EmbedBuilder simplePlayerEmbed(OfflinePlayer player, String title, Color color)
+	public static EmbedBuilder simplePlayerEmbed(OfflinePlayer player, String playerDidWhat, Color color)
 	{
 		EmbedBuilder eb = new EmbedBuilder();
 		
 		// Thank you to "https://crafatar.com" for providing avatars.
 		eb.setColor(color);
-		eb.setAuthor(player.getName() + " " + title, null, "https://crafatar.com/avatars/" + player.getUniqueId());
+		eb.setAuthor(player.getName() + " " + playerDidWhat, null, "https://crafatar.com/avatars/" + player.getUniqueId());
 		
 		return eb;
 	}
@@ -137,12 +131,12 @@ public class Discord extends ListenerAdapter {
 	 * @param color The color
 	 * @return the EmbedBuilder
 	 */
-	public static EmbedBuilder simpleServerEmbed(String title, String icon, Color color)
+	public static EmbedBuilder simpleServerEmbed(String author, String iconLink, Color color)
 	{
 		EmbedBuilder eb = new EmbedBuilder();
 		
 		eb.setColor(color);
-		eb.setAuthor(title, null, icon);
+		eb.setAuthor(author, null, iconLink);
 		
 		return eb;
 	}
@@ -151,7 +145,7 @@ public class Discord extends ListenerAdapter {
 	{
 		if (id == null) return null;
 		
-		return JDA.getGuildById(PGF_ID)
+		return getGuildPGF()
 		.getMemberById(id)
 		.getRoles().stream()
 		.map(role -> role.getId())
