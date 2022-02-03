0b513791fb0b816676b752cb8b4e0e434b5f9cda
bk <bk123yt@gmail.com>
2022-02-02 20:49:49 -0600

    Fixed double player join for #alerts

--- Bot/src/net/pgfmc/bot/listeners/OnMemberJoin.java
+++ Bot/src/net/pgfmc/bot/listeners/OnMemberJoin.java
@@ -9,6 +9,8 @@ import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 import net.pgfmc.bot.Discord;
 import net.pgfmc.bot.functions.spam.Raid;
+import net.pgfmc.core.CoreMain;
+import net.pgfmc.core.CoreMain.Machine;
 
 public class OnMemberJoin implements EventListener {
 
@@ -17,6 +19,9 @@ public class OnMemberJoin implements EventListener {
 		if (!(event instanceof GuildMemberJoinEvent)) return;
 		
 		GuildMemberJoinEvent e = (GuildMemberJoinEvent) event;
+		
+		if (CoreMain.machine != Machine.MAIN) return;
+		
 		User user = e.getUser();
 		
 		EmbedBuilder eb = Discord.simpleServerEmbed(user.getAsTag() + " has joined PGF!", user.getEffectiveAvatarUrl(), Discord.gold);
