28ccefc9c4f98f6da5d21577527734ed69123e39
bk <bk123yt@gmail.com>
2022-02-03 01:28:53 -0600

    Fixed broadcast

--- Core/src/net/pgfmc/core/cmd/admin/Broadcast.java
+++ Core/src/net/pgfmc/core/cmd/admin/Broadcast.java
@@ -1,9 +1,13 @@
 package net.pgfmc.core.cmd.admin;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+
+import net.pgfmc.bot.Discord;
+import net.pgfmc.core.CoreMain;
 
 public class Broadcast implements CommandExecutor {
 
@@ -15,8 +19,14 @@ public class Broadcast implements CommandExecutor {
 			return false;
 		}
 		
-		String msg = String.join(" ", args).replace("&", "§");
+		String msg = ChatColor.GRAY + "["
+					+ ChatColor.BOLD + ChatColor.LIGHT_PURPLE + "PGF"
+					+ ChatColor.GRAY + "] "
+					+ ChatColor.RESET + ChatColor.DARK_RED + String.join(" ", args).replace('&', ChatColor.COLOR_CHAR);
 		Bukkit.broadcastMessage(msg);
+		
+		if (!CoreMain.PGFPlugin.BOT.isEnabled()) return true;
+		Discord.sendMessage("```" + ChatColor.stripColor(msg) + "```");
 		
 		return true;
 	}
