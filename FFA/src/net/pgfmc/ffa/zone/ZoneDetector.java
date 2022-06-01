db1b748047c814907bcffeb8fc4acbfeb9fa3ec0
bk <bk123yt@gmail.com>
2022-06-01 05:52:13 -0500

    Zone inventories, added ZoneInfo
    Ability to set zone inventories with in-game command, added class ZoneInfo for util methods

---
+++ FFA/src/net/pgfmc/ffa/zone/ZoneDetector.java
@@ -0,0 +1,15 @@
+package net.pgfmc.ffa.zone;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class ZoneDetector implements Listener {
+	
+	@EventHandler
+	public void onMove(PlayerMoveEvent e)
+	{
+		// TODO
+	}
+	
+}
