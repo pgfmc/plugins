da81f11655ab593eed934c9e4e627659e0fbf78f
bk <33919755+bkiscool@users.noreply.github.com>
2022-06-01 07:52:25 -0500

    QOL fixes, added sounds/polish
    Changed how switching zones works/zone detection, added sound effects and messages when someone is killed

--- FFA/src/net/pgfmc/ffa/zone/ZoneDetector.java
+++ FFA/src/net/pgfmc/ffa/zone/ZoneDetector.java
@@ -1,15 +1,20 @@
 package net.pgfmc.ffa.zone;
 
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+
+import net.pgfmc.ffa.zone.ZoneInfo.Zone;
 
 public class ZoneDetector implements Listener {
 	
 	@EventHandler
 	public void onMove(PlayerMoveEvent e)
 	{
-		// TODO
+		if (e.getPlayer().getGameMode() != GameMode.SURVIVAL) return;
+		
+		Zone.switchZoneUknown(e.getPlayer());
 	}
 	
 }
