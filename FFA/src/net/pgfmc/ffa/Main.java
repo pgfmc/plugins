84e8993c9c794171bc92cf9f304083adfa876ea7
bk <bk123yt@gmail.com>
2022-06-01 05:52:13 -0500

    Zone inventories, added ZoneInfo
    Ability to set zone inventories with in-game command, added class ZoneInfo for util methods

--- FFA/src/net/pgfmc/ffa/Main.java
+++ FFA/src/net/pgfmc/ffa/Main.java
@@ -2,12 +2,18 @@ package net.pgfmc.ffa;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.pgfmc.ffa.cmd.SetZoneInventory;
+
 public class Main extends JavaPlugin {
+	
+	public static Main plugin;
 	
 	@Override
 	public void onEnable()
 	{
+		plugin = this;
 		
+		getCommand("setzoneinventory").setExecutor(new SetZoneInventory());
 	}
 	
 	@Override
---
+++ FFA/src/net/pgfmc/ffa/zone/ZoneInfo.java
@@ -0,0 +1,85 @@
+package net.pgfmc.ffa.zone;
+
+import java.io.File;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+
+import net.pgfmc.core.file.Configify;
+import net.pgfmc.core.file.Mixins;
+import net.pgfmc.ffa.Main;
+
+public class ZoneInfo extends Configify {
+	
+	public enum Zone {
+		Safe,
+		Combat;
+		
+		Inventory inventory;
+		
+		public void setInventoryItems(Inventory inventory)
+		{
+			this.inventory = inventory;
+		}
+		
+		public Inventory getInventory()
+		{
+			return inventory;
+		}
+		
+		public void switchZone(Player player)
+		{
+			player.getInventory().setContents(inventory.getContents());
+		}
+
+	}
+	
+	public ZoneInfo()
+	{
+		super(Mixins.getFile(Main.plugin.getDataFolder() + File.separator + "zone-items.yml"));
+		
+		setDefaultValue("safe", Bukkit.createInventory(null, InventoryType.PLAYER));
+		setDefaultValue("combat", Bukkit.createInventory(null, InventoryType.PLAYER));
+	}
+	
+	public static Zone getZoneFromLocation(Location loc)
+	{
+		int x = loc.getBlockX();
+		int z = loc.getBlockZ();
+		
+		if (x >= 10 || z >= 10) return Zone.Combat;
+		
+		return Zone.Safe;
+		
+	}
+	
+	
+	
+	@Override
+	public void reload() {
+		FileConfiguration config = getConfig();
+		
+		Zone.Safe.setInventoryItems((Inventory) config.get("safe"));
+		Zone.Combat.setInventoryItems((Inventory) config.get("combat"));
+		
+	}
+
+	@Override
+	public void enable() {
+		reload();
+	}
+
+	@Override
+	public void disable() {
+		FileConfiguration config = getConfig();
+		
+		config.set("safe", Zone.Safe.getInventory());
+		config.set("combat", Zone.Combat.getInventory());
+		
+		save(config);
+	}
+}
