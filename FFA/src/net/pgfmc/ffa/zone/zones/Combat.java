855400ba080c4687bc0c81a28eab332722eaaa6e
bk <bk123yt@gmail.com>
2022-06-02 07:07:43 -0500

    Fixed spawn loc, Fixed unwanted damage
    * Can only die from player
    * Projectiles will harm player if shot from Player
    * Fall damage is off

--- FFA/src/net/pgfmc/ffa/zone/zones/Combat.java
+++ FFA/src/net/pgfmc/ffa/zone/zones/Combat.java
@@ -7,14 +7,18 @@ import org.bukkit.Sound;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
 import net.pgfmc.core.playerdataAPI.PlayerData;
 import net.pgfmc.ffa.zone.ZoneDo;
 import net.pgfmc.ffa.zone.ZoneInfo;
 import net.pgfmc.ffa.zone.ZoneInfo.Zone;
 
-public class Combat implements ZoneDo {
+public class Combat implements ZoneDo, Listener {
 	
 	@Override
 	public void zoneDo(EntityDamageByEntityEvent e)
@@ -33,7 +37,7 @@ public class Combat implements ZoneDo {
 			p.setHealth(p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue());
 			p.setSaturation(20f); // ???? look into
 			
-			p.teleport(new Location(p.getWorld(), 0, 0, 0)); // Spawn/lounge
+			p.teleport(new Location(p.getWorld(), 0, 104, 0)); // Spawn/lounge
 			
 			Player damager = null;
 			
@@ -62,6 +66,15 @@ public class Combat implements ZoneDo {
 			pdDamager.playSound(Sound.ENTITY_EXPERIENCE_ORB_PICKUP);
 			
 		}
+	}
+	
+	@EventHandler
+	public void onDamage(EntityDamageEvent e)
+	{
+		if (e.getCause() == DamageCause.ENTITY_ATTACK || e.getCause() == DamageCause.PROJECTILE) return;
+		if (!(e.getEntity() instanceof Player)) return;
+		
+		e.setCancelled(true);
 	}
 
 }
