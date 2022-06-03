8247b3ea108dfa13828efc73feef158a4868d19e
bk <bk123yt@gmail.com>
2022-06-03 02:42:51 -0500

    K/D and no hostile entity damage
    Added kill death ratio scoreboard under players' nameplates
    
    Fixed damage being taken from non players

--- FFA/src/net/pgfmc/ffa/Main.java
+++ FFA/src/net/pgfmc/ffa/Main.java
@@ -1,13 +1,20 @@
 package net.pgfmc.ffa;
 
+import java.util.Optional;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.vehicle.VehicleDamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.pgfmc.core.playerdataAPI.PlayerDataManager;
 import net.pgfmc.ffa.cmd.handler.FFACmdHandler;
 import net.pgfmc.ffa.zone.ZoneDetector;
 import net.pgfmc.ffa.zone.ZoneInfo;
 import net.pgfmc.ffa.zone.zones.Combat;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
 	
 	public static Main plugin;
 	
@@ -16,6 +23,18 @@ public class Main extends JavaPlugin {
 	{
 		plugin = this;
 		
+		PlayerDataManager.setInit(pd -> {
+			
+			FileConfiguration db = pd.loadFile();
+			
+			double kills = Optional.ofNullable(db.getDouble("kills")).orElse(0.0);
+			double deaths = Optional.ofNullable(db.getDouble("deaths")).orElse(0.0);
+			
+			pd.setData("kills", kills).queue();
+			pd.setData("deaths", deaths).queue();
+			
+		});
+		
 		getCommand("ffa").setExecutor(new FFACmdHandler());
 		getServer().getPluginManager().registerEvents(new ZoneDetector(), this);
 		getServer().getPluginManager().registerEvents(new Combat(), this);
@@ -23,10 +42,10 @@ public class Main extends JavaPlugin {
 		new ZoneInfo(); // Needed for Configify to work apparently ?
 	}
 	
-	@Override
-	public void onDisable()
+	@EventHandler
+	public void onInteractMinecart(VehicleDamageEvent e)
 	{
-		
+		e.setCancelled(true);
 	}
 
 }
