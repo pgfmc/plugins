fe69a84ef27a942b38b2ea985bcfae8e1ed911d8
CrimsonDart <79547580+CrimsonDart@users.noreply.github.com>
2022-06-07 15:45:43 -0500

    added pvp stuff (untested)

--- Survival/src/net/pgfmc/survival/Main.java
+++ Survival/src/net/pgfmc/survival/Main.java
@@ -3,6 +3,7 @@ package net.pgfmc.survival;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import net.pgfmc.survival.cmd.Afk;
+import net.pgfmc.survival.cmd.PvpToggle;
 import net.pgfmc.survival.cmd.donator.Craft;
 import net.pgfmc.survival.cmd.donator.Echest;
 
@@ -18,11 +19,12 @@ public class Main extends JavaPlugin {
 		getCommand("afk").setExecutor(new Afk());
 		getCommand("echest").setExecutor(new Echest());
 		getCommand("craft").setExecutor(new Craft());
+		new PvpToggle();
 		
 		
 		getServer().getPluginManager().registerEvents(new Afk(), this);
 		getServer().getPluginManager().registerEvents(new ItemProtect(), this);
-		
+		getServer().getPluginManager().registerEvents(new PvPEvent(), this);
 	}
 	
 	@Override
@@ -30,5 +32,4 @@ public class Main extends JavaPlugin {
 	{
 		
 	}
-
 }
