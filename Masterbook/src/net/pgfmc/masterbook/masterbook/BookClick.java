d2ebc6818a76777ed96dd9bdb850fc2697ac919b
CrimsonDart <79547580+CrimsonDart@users.noreply.github.com>
2022-06-09 01:57:18 -0500

    Right clicking with a book now works with command menu

--- Masterbook/src/net/pgfmc/masterbook/masterbook/BookClick.java
+++ Masterbook/src/net/pgfmc/masterbook/masterbook/BookClick.java
@@ -1,6 +1,5 @@
 package net.pgfmc.masterbook.masterbook;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -15,18 +14,14 @@ public class BookClick implements Listener {
 	@EventHandler
 	public void bookClick(PlayerInteractEvent e) {
 		
-		if (e.getAction() != Action.RIGHT_CLICK_AIR || e.getAction() != Action.RIGHT_CLICK_BLOCK) return;
-		Bukkit.getLogger().info("0");
-		if (!e.hasItem()) return;
-		Bukkit.getLogger().info("1");
-		Player p = e.getPlayer();
-		Bukkit.getLogger().info("2");
-		if (p.isSneaking()) return;
-		Bukkit.getLogger().info("3");
-		if (p.getInventory().getItemInMainHand().getType() != Material.BOOK) return;
-		Bukkit.getLogger().info("4");
-		
-		p.closeInventory();
-		p.openInventory(new CommandsMenu(PlayerData.from(p)).getInventory());
+		if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			if (!e.hasItem()) return;
+			Player p = e.getPlayer();
+			if (p.isSneaking()) return;
+			if (p.getInventory().getItemInMainHand().getType() != Material.BOOK) return;
+			
+			p.closeInventory();
+			p.openInventory(new CommandsMenu(PlayerData.from(p)).getInventory());
+		}
 	}
 }
