27ec42a52e8830e57f73a286b5913b6c52268478
bk <33919755+bkiscool@users.noreply.github.com>
2022-06-09 01:31:12 -0500

    Removed FFA
    Deleted FFA

--- Masterbook/src/net/pgfmc/masterbook/masterbook/BookClick.java
+++ Masterbook/src/net/pgfmc/masterbook/masterbook/BookClick.java
@@ -1,5 +1,6 @@
 package net.pgfmc.masterbook.masterbook;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,16 +15,16 @@ public class BookClick implements Listener {
 	@EventHandler
 	public void bookClick(PlayerInteractEvent e) {
 		
-		if (e.getAction() != Action.RIGHT_CLICK_AIR && e.getAction() != Action.RIGHT_CLICK_BLOCK) return;
-		System.out.print("0");
+		if (e.getAction() != Action.RIGHT_CLICK_AIR || e.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+		Bukkit.getLogger().info("0");
 		if (!e.hasItem()) return;
-		System.out.print("1");
+		Bukkit.getLogger().info("1");
 		Player p = e.getPlayer();
-		System.out.print("2");
+		Bukkit.getLogger().info("2");
 		if (p.isSneaking()) return;
-		System.out.print("3");
+		Bukkit.getLogger().info("3");
 		if (p.getInventory().getItemInMainHand().getType() != Material.BOOK) return;
-		System.out.print("4");
+		Bukkit.getLogger().info("4");
 		
 		p.closeInventory();
 		p.openInventory(new CommandsMenu(PlayerData.from(p)).getInventory());
