0c4343416f2530e6011c203b6175845ae9c8a248
bk <33919755+bkiscool@users.noreply.github.com>
2023-02-03 10:31:53 -0600

    Added color for size of inventory
    * Different colors = amount of items in inventory
    * Renamed 3 class files to match the command name change
    * Saves inventory now instead of the contents (probably bad idk)
    * Still need to make it better

--- ModTools/src/net/pgfmc/modtools/rollback/inv/inv/InventoryBackupList.java
+++ ModTools/src/net/pgfmc/modtools/rollback/inv/inv/InventoryBackupList.java
@@ -1,8 +1,10 @@
 package net.pgfmc.modtools.rollback.inv.inv;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.bukkit.Material;
 import org.bukkit.event.inventory.InventoryType;
@@ -13,12 +15,12 @@ import net.pgfmc.core.api.inventory.ListInventory;
 import net.pgfmc.core.api.inventory.extra.Butto;
 import net.pgfmc.core.api.playerdata.PlayerData;
 import net.pgfmc.core.util.ItemWrapper;
-import net.pgfmc.modtools.rollback.RollbackBackup;
-import net.pgfmc.modtools.rollback.RollbackScheduler;
+import net.pgfmc.modtools.rollback.InventoryBackup;
+import net.pgfmc.modtools.rollback.InventoryBackupScheduler;
 import net.pgfmc.modtools.rollback.inv.InventoryOnlinePlayersList;
 import net.pgfmc.modtools.rollback.inv.inv.inv.InventoryBackupConfirm;
 
-public class InventoryBackupList extends ListInventory<RollbackBackup> {
+public class InventoryBackupList extends ListInventory<InventoryBackup> {
 	
 	PlayerData pd;
 
@@ -32,13 +34,13 @@ public class InventoryBackupList extends ListInventory<RollbackBackup> {
 
 	@SuppressWarnings("unchecked")
 	@Override
-	protected List<RollbackBackup> load() {		
-		return (List<RollbackBackup>) Optional.ofNullable(((List<RollbackBackup>) pd.getData("inventories")))
-		.orElse(new ArrayList<RollbackBackup>());
+	protected List<InventoryBackup> load() {		
+		return (List<InventoryBackup>) Optional.ofNullable(((List<InventoryBackup>) pd.getData("inventories")))
+		.orElse(new ArrayList<InventoryBackup>());
 	}
 
 	@Override
-	protected Butto toAction(RollbackBackup entry) {
+	protected Butto toAction(InventoryBackup entry) {
 		
 		return (p, e) -> {
 			if (!p.hasPermission("pgf.admin.inventory.restore"))
@@ -48,15 +50,63 @@ public class InventoryBackupList extends ListInventory<RollbackBackup> {
 			}
 			
 			p.closeInventory();
-			p.openInventory(new InventoryBackupConfirm(RollbackScheduler.INVENTORY_DATE_FORMAT.format(entry.getDate()) + " - " + entry.getCause().name(), entry).getInventory());
+			p.openInventory(new InventoryBackupConfirm(InventoryBackupScheduler.INVENTORY_DATE_FORMAT.format(entry.getDate()) + " - " + entry.getCause().name(), entry).getInventory());
 			
 		};
 		
 	}
 
 	@Override
-	protected ItemStack toItem(RollbackBackup entry) {
-		return new ItemWrapper(Material.WRITABLE_BOOK).n(RollbackScheduler.INVENTORY_DATE_FORMAT.format(entry.getDate())).l(entry.getCause().name()).gi();
+	protected ItemStack toItem(InventoryBackup entry) {
+		final int itemStackCount = Arrays.asList(entry.getInventory().getContents())
+								.stream()
+								.filter(item -> item != null)
+								.collect(Collectors.toList())
+								.size();
+		
+		if (itemStackCount == 0)
+		{
+			return new ItemWrapper(Material.WHITE_DYE)
+					.n(InventoryBackupScheduler.INVENTORY_DATE_FORMAT.format(entry.getDate()))
+					.l(entry.getCause().name())
+					.gi();
+		}
+		
+		if (itemStackCount <= 9)
+		{
+			return new ItemWrapper(Material.GRAY_DYE)
+					.n(InventoryBackupScheduler.INVENTORY_DATE_FORMAT.format(entry.getDate()))
+					.l(entry.getCause().name())
+					.gi();
+		}
+		
+		if (itemStackCount <= 18)
+		{
+			return new ItemWrapper(Material.YELLOW_DYE)
+					.n(InventoryBackupScheduler.INVENTORY_DATE_FORMAT.format(entry.getDate()))
+					.l(entry.getCause().name())
+					.gi();
+		}
+		
+		if (itemStackCount < 27)
+		{
+			return new ItemWrapper(Material.ORANGE_DYE)
+					.n(InventoryBackupScheduler.INVENTORY_DATE_FORMAT.format(entry.getDate()))
+					.l(entry.getCause().name())
+					.gi();
+		}
+		
+		if (itemStackCount == 27)
+		{
+			return new ItemWrapper(Material.RED_DYE)
+					.n(InventoryBackupScheduler.INVENTORY_DATE_FORMAT.format(entry.getDate()))
+					.l(entry.getCause().name())
+					.gi();
+		}
+		
+		return new ItemWrapper(Material.BARRIER).gi(); // This shouldn't happen lol
+		
+		
 	}
 
 }
