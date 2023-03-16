ab3f54c4f4e6565c4530f14abddd5815725f8077
CrimsonDart <james@jameslondon.me>
2023-03-15 20:52:58 -0500

    Added Parkour plugin, but not much code :/

---
+++ Parkour/src/net/pgfmc/parkour/Main.java
@@ -0,0 +1,18 @@
+package pgf.plugins.Parkour.src.net.pgfmc.parkour;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+public class Main extends JavaPlugin {
+    public static Main plugin;
+
+    @Override
+    public void onEnable() {
+
+        plugin = this;
+
+
+
+    }
+}
