683efc89261c8d6c02b6fdf6ffafd9a04fa3461f
bk <33919755+bkiscool@users.noreply.github.com>
2023-12-23 02:16:13 -0600

    Particle effect cosmetics
    Starting creating the particle effect cosmetics
    Added:
    * Halo Note
    * Halo Heart
    * Halo Flame

---
+++ Survival/src/net/pgfmc/survival/particleeffects/HaloHeart.java
@@ -0,0 +1,13 @@
+package net.pgfmc.survival.particleeffects;
+
+import org.bukkit.Particle;
+
+public class HaloHeart extends HaloParticleEffect {
+
+	public HaloHeart() {
+		super(Particle.HEART);
+		
+		animate();
+	}
+
+}
