12c557236610aa7c81573ee9d1a89d53959a33d3
bk <33919755+bkiscool@users.noreply.github.com>
2023-12-31 01:05:20 -0600

    Pvp toggle message is sent to Discord

--- Survival/src/net/pgfmc/survival/cmd/pvp/Pvp.java
+++ Survival/src/net/pgfmc/survival/cmd/pvp/Pvp.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.bukkit.ChatColor;
 
 import net.pgfmc.core.api.playerdata.PlayerData;
+import net.pgfmc.core.bot.discord.Discord;
 import net.pgfmc.core.util.ServerMessage;
 import net.pgfmc.core.util.commands.PlayerCommand;
 import net.pgfmc.core.util.roles.PGFRoles;
@@ -26,14 +27,16 @@ public class Pvp extends PlayerCommand {
 		if (pd.hasTag("pvp")) {
 			pd.removeTag("pvp");
 			
-			ServerMessage.sendServerMessage(pd.getRankedName() + ChatColor.GREEN + " disabled PVP.");
+			ServerMessage.sendServerMessage(pd.getRankedName() + ChatColor.GREEN + " disabled PVP");
+			Discord.sendMessage(ChatColor.stripColor(":shield: " + pd.getRankedName() + " disabled PVP")).queue();;
 			
 			PGFRoles.updatePlayerNameplate(pd);
 			
 		} else {
 			pd.addTag("pvp");
 			
-			ServerMessage.sendServerMessage(pd.getRankedName() + ChatColor.RED + " enabled PVP!");
+			ServerMessage.sendServerMessage(pd.getRankedName() + ChatColor.RED + " enabled PVP");
+			Discord.sendMessage(ChatColor.stripColor(":crossed_swords: " + pd.getRankedName() + " enabled PVP")).queue();;
 			
 			PGFRoles.updatePlayerNameplate(pd);
 		}
