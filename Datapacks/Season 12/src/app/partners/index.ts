0115f10517593dde0998785f266514604b394faa
bk <33919755+bkiscool@users.noreply.github.com>
2024-05-13 20:24:32 -0500

    Update to 1.20.6

--- Datapacks/Season 12/src/app/partners/index.ts
+++
@@ -1,8 +0,0 @@
-import type { CollectionRegistry, SchemaRegistry } from '@mcschema/core'
-import { initImmersiveWeathering } from './ImmersiveWeathering.js'
-
-export * from './ImmersiveWeathering.js'
-
-export function initPartners(schemas: SchemaRegistry, collections: CollectionRegistry) {
-	initImmersiveWeathering(schemas, collections)
-}
