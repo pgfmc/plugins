922e49ea587a921a78153b22da8dddab6e0eac29
CrimsonDart <james@jameslondon.me>
2024-05-24 02:21:53 +0000

    closes #174

--- Survival/src/net/pgfmc/survival/menu/CommandMenuInventory.java
+++ Survival/src/net/pgfmc/survival/menu/CommandMenuInventory.java
@@ -45,10 +45,12 @@ public class CommandMenuInventory implements InventoryHolder {
 		public Homepage()
 		{
 			super(InventoryType.CHEST, "Command Menu");
-			
+
+            
+			final int afk_location = 3;
 			/* 
 			 * AFK Toggle
-			 * [] [] XX [] [] [] [] [] []
+			 * [] [] [] XX [] [] [] [] []
 			 * [] [] [] [] [] [] [] [] []
 			 * [] [] [] [] [] [] [] [] []
 			 */
@@ -56,25 +58,24 @@ public class CommandMenuInventory implements InventoryHolder {
 			{
 				if (playerdata.hasTag("afk"))
 				{
-					setAction(2, (player, event) -> {
+					setAction(afk_location, (player, event) -> {
 						player.performCommand("afk");
 						player.openInventory(new CommandMenuInventory(playerdata).getInventory());
 						
 					});
 					
-					setItem(2, Material.BLUE_ICE).n(ChatColor.GRAY + "AFK: " + ChatColor.GREEN + "Enabled")
+					setItem(afk_location, Material.BLUE_ICE).n(ChatColor.GRAY + "AFK: " + ChatColor.GREEN + "Enabled")
 												.l(ChatColor.GRAY + "Take no damage while enabled.");
 					
 				} else if (!playerdata.hasTag("afk")) {
-					setAction(2, (player, event) -> {
+					setAction(afk_location, (player, event) -> {
 						player.performCommand("afk");
 						player.openInventory(new CommandMenuInventory(playerdata).getInventory());
 						
 					});
 					
-					setItem(2, Material.GLASS).n(ChatColor.GRAY + "AFK: " + ChatColor.RED + "Disabled")
+					setItem(afk_location, Material.GLASS).n(ChatColor.GRAY + "AFK: " + ChatColor.RED + "Disabled")
 											.l(ChatColor.GRAY + "Take no damage while enabled.");
-					
 				}
 				
 			}
@@ -85,22 +86,22 @@ public class CommandMenuInventory implements InventoryHolder {
 			 * [] [] [] [] [] [] [] [] []
 			 * [] [] [] [] XX [] [] [] []
 			 */
-			if (playerdata.getData("backLoc") == null)
-			{
-				setAction(22, (player, event) -> {
-					playerdata.playSound(Sound.BLOCK_NOTE_BLOCK_BASS);
-					playerdata.sendMessage(ChatColor.RED + "No back location available.");
-				});
-			
-			} else {
-				setAction(22, (player, event) -> {
-					player.openInventory(new BackConfirmInventory(playerdata).getInventory());
-				});
-				
-			}
-			
-			setItem(22, Material.SPECTRAL_ARROW).n(ChatColor.DARK_GREEN + "Teleport Back")
-			.l(ChatColor.GRAY + "Go back to where you teleported from.");
+			//if (playerdata.getData("backLoc") == null)
+			//{
+			//	setAction(22, (player, event) -> {
+			//		playerdata.playSound(Sound.BLOCK_NOTE_BLOCK_BASS);
+			//		playerdata.sendMessage(ChatColor.RED + "No back location available.");
+			//	});
+			//
+			//} else {
+			//	setAction(22, (player, event) -> {
+			//		player.openInventory(new BackConfirmInventory(playerdata).getInventory());
+			//	});
+			//	
+			//}
+		    //	
+			//setItem(22, Material.SPECTRAL_ARROW).n(ChatColor.DARK_GREEN + "Teleport Back")
+			//.l(ChatColor.GRAY + "Go back to where you teleported from.");
 			
 			
 			/* 
@@ -109,52 +110,46 @@ public class CommandMenuInventory implements InventoryHolder {
 			 * [] [] [] [] [] [] [] [] []
 			 * [] [] XX [] [] [] [] [] []
 			 */
-			setAction(20, (player, event) -> {
-				player.openInventory(new HomeHomepageInventory(playerdata).getInventory());
-			});
-					
-			setItem(20, Material.RED_BED).n(ChatColor.YELLOW + "Home Menu");
+			//setAction(20, (player, event) -> {
+			//	player.openInventory(new HomeHomepageInventory(playerdata).getInventory());
+			//});
+			//		
+			//setItem(20, Material.RED_BED).n(ChatColor.YELLOW + "Home Menu");
 			
 			
+            final int teleports = 13;
 			/* 
 			 * Teleport Menu
-			 * [] [] [] [] [] [] [] [] []
-			 * [] [] [] [] [] [] [] [] []
-			 * [] [] [] XX [] [] [] [] []
-			 */
-			if (Bukkit.getOnlinePlayers().size() > 1)
-			{
-				setAction(21, (player, event) -> {
-					player.openInventory(new TpaListInventory(playerdata).getInventory());
-				});
-			
-				setItem(21, Material.ENDER_PEARL).n(ChatColor.DARK_PURPLE + "Teleport Menu")
-												.l(ChatColor.GRAY + "Teleport to another player.");
-				
-			} else {
-				setAction(21, (player, event) -> {
-					playerdata.playSound(Sound.BLOCK_NOTE_BLOCK_BASS);
-				});
-				
-				setItem(21, Material.ENDER_EYE).n(ChatColor.DARK_PURPLE + "Teleport Menu")
-												.l(ChatColor.RED + "No players online.");
-				
-			}
-			
-			/* 
-			 * Profile
 			 * [] [] [] [] [] [] [] [] []
 			 * [] [] [] [] XX [] [] [] []
 			 * [] [] [] [] [] [] [] [] []
 			 */
-			setAction(13, (player, event) -> {
+			if (Bukkit.getOnlinePlayers().size() > 1) {
+
+                // TODO
+
+				setAction(teleports, (player, event) -> {
+					player.openInventory(new TpaListInventory(playerdata).getInventory());
+				});
+			
+				setItem(teleports, Material.ENDER_PEARL).n(ChatColor.DARK_PURPLE + "Teleport Menu")
+												.l(ChatColor.GRAY + "Different ways to get around.");
+			}
+			
+            final int profile = 5;
+			/* 
+			 * Profile
+			 * [] [] [] [] [] XX [] [] []
+			 * [] [] [] [] [] [] [] [] []
+			 * [] [] [] [] [] [] [] [] []
+			 */
+			setAction(profile, (player, event) -> {
 				player.openInventory(new ProfileInventory(playerdata).getInventory());
-				
 			});
 			
 			final PGFRole role = RoleManager.getPlayerTopRole(playerdata);
 			
-			setItem(13, Skull.getHead(playerdata.getUniqueId()))
+			setItem(profile, Skull.getHead(playerdata.getUniqueId()))
 					.n(playerdata.getRankedName() + " (" + role.getName().substring(0,1).toUpperCase() + role.getName().substring(1).toLowerCase() + ")")
 					.l(ChatColor.GRAY + "Open Profile.");
 			
@@ -182,28 +177,29 @@ public class CommandMenuInventory implements InventoryHolder {
 			setItem(23, Material.ENDER_CHEST).n(ChatColor.DARK_AQUA + "Ender Chest")
 											.l(ChatColor.BLUE + "Open an Ender Chest. Donator perk!");
 			
+            final int crafting = 22;
 			/* 
 			 * Crafting Table
 			 * [] [] [] [] [] [] [] [] []
 			 * [] [] [] [] [] [] [] [] []
-			 * [] [] [] [] [] [] XX [] []
+			 * [] [] [] [] XX [] [] [] []
 			 */
 			if (playerdata.hasPermission("net.pgfmc.survival.craft"))
 			{
-				setAction(24, (player, event) -> {
+				setAction(crafting, (player, event) -> {
 					player.performCommand("craft");
 				});
 				
 			} else
 			{
-				setAction(24, (player, event) -> {
+				setAction(crafting, (player, event) -> {
 					playerdata.playSound(Sound.BLOCK_NOTE_BLOCK_BASS);
 					playerdata.sendMessage(ChatColor.RED + "Only donators can use this command.");
 				});
 				
 			}
 			
-			setItem(24, Material.CRAFTING_TABLE).n(ChatColor.DARK_AQUA + "Crafting Table")
+			setItem(crafting, Material.CRAFTING_TABLE).n(ChatColor.DARK_AQUA + "Crafting Table")
 											.l(ChatColor.BLUE + "Open a Crafting Table. Donator perk!");
 			
 			
@@ -215,40 +211,40 @@ public class CommandMenuInventory implements InventoryHolder {
 			 */
 			
 			// Get all incoming requests for this player
-			final Set<Request> requests = RequestType.getInAllRequests(request -> request.target == playerdata);
+		//	final Set<Request> requests = RequestType.getInAllRequests(request -> request.target == playerdata);
+		//	
+		//	if (requests == null || requests.isEmpty()) // no incoming requests
+		//	{
+		//		setAction(5, (player, event) -> {
+		//			playerdata.sendMessage(ChatColor.RED + "There are no incoming requests.");
+		//			playerdata.playSound(Sound.BLOCK_NOTE_BLOCK_BASS);
+		//			
+		//		});
+		//		
+		//		setItem(5, Material.BOOK).n(ChatColor.DARK_RED + "Requests Menu (0)");
+		//		
+		//	} else // there are incoming requests
+		//	{
+		//		// number of incoming requests
+		//		final int requestsCount = requests.size();
+		//		
+		//		setAction(5, (player, event) -> {
+		//			final RequestListInventory inv = new RequestListInventory(playerdata);
+		//			inv.setBack(0, new CommandMenuInventory(playerdata).getInventory());
+		//			
+		//			player.openInventory(inv.getInventory());
+		//			
+		//		});
+		//		
+		//		setItem(5, Material.WRITABLE_BOOK).n(ChatColor.DARK_RED + "Requests Menu (" + requestsCount + ")");
+		//		
+		//	}
 			
-			if (requests == null || requests.isEmpty()) // no incoming requests
-			{
-				setAction(5, (player, event) -> {
-					playerdata.sendMessage(ChatColor.RED + "There are no incoming requests.");
-					playerdata.playSound(Sound.BLOCK_NOTE_BLOCK_BASS);
-					
-				});
-				
-				setItem(5, Material.BOOK).n(ChatColor.DARK_RED + "Requests Menu (0)");
-				
-			} else // there are incoming requests
-			{
-				// number of incoming requests
-				final int requestsCount = requests.size();
-				
-				setAction(5, (player, event) -> {
-					final RequestListInventory inv = new RequestListInventory(playerdata);
-					inv.setBack(0, new CommandMenuInventory(playerdata).getInventory());
-					
-					player.openInventory(inv.getInventory());
-					
-				});
-				
-				setItem(5, Material.WRITABLE_BOOK).n(ChatColor.DARK_RED + "Requests Menu (" + requestsCount + ")");
-				
-			}
 			
-			
-			
+		    final int pvp = 4;	
 			/* 
 			 * PVP Toggle
-			 * [] [] [] XX [] [] [] [] []
+			 * [] [] [] [] XX [] [] [] []
 			 * [] [] [] [] [] [] [] [] []
 			 * [] [] [] [] [] [] [] [] []
 			 */
@@ -256,86 +252,89 @@ public class CommandMenuInventory implements InventoryHolder {
 			{
 				if (playerdata.hasTag("pvp"))
 				{
-					setAction(3, (player, event) -> {
+					setAction(pvp, (player, event) -> {
 						player.performCommand("pvp");
 						player.openInventory(new CommandMenuInventory(playerdata).getInventory());
 						playerdata.playSound(Sound.BLOCK_NOTE_BLOCK_BASS);
 					});
 					
-					setItem(3, Material.DIAMOND_SWORD).n(ChatColor.GRAY + "PVP: " + ChatColor.DARK_RED + "Enabled")
+					setItem(pvp, Material.DIAMOND_SWORD).n(ChatColor.GRAY + "PVP: " + ChatColor.DARK_RED + "Enabled")
 														.l(ChatColor.GRAY + "Fight other players while enabled.");
 					
 				} else {
-					setAction(3, (player, event) -> {
+					setAction(pvp, (player, event) -> {
 						player.performCommand("pvp");
 						player.openInventory(new CommandMenuInventory(playerdata).getInventory());
 						playerdata.playSound(playerdata.getPlayer().getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1.0F, 2.0F);
 					});
 					
-					setItem(3, Material.WOODEN_SWORD).n(ChatColor.GRAY + "PVP: Disabled")
+					setItem(pvp, Material.WOODEN_SWORD).n(ChatColor.GRAY + "PVP: Disabled")
 													.l(ChatColor.GRAY + "Fight other players while enabled.");
 					
 				}
 				
 			}
 			
+            final int rewards = 16;
 			/* 
 			 * Rewards
-			 * [] [] [] [] [] [] XX [] []
 			 * [] [] [] [] [] [] [] [] []
+			 * [] [] [] [] [] [] XX [] []
 			 * [] [] [] [] [] [] [] [] []
 			 */
 			final int numberOfRewards = Rewards.getPlayerRewardsMap(playerdata).size();
 			
 			if (numberOfRewards == 0)
 			{
-				setAction(6, (player, event) -> {
+				setAction(rewards, (player, event) -> {
 					playerdata.sendMessage(ChatColor.RED + "There are no rewards currently.");
 					playerdata.playSound(Sound.BLOCK_NOTE_BLOCK_BASS);
 				});
 				
-				setItem(6, Material.CHISELED_BOOKSHELF).n(ChatColor.YELLOW + "Rewards (" + numberOfRewards + ")");
+				setItem(rewards, Material.CHISELED_BOOKSHELF).n(ChatColor.YELLOW + "Rewards (" + numberOfRewards + ")");
 				
 			} else
 			{
-				setAction(6, (player, event) -> {
+				setAction(rewards, (player, event) -> {
 					player.openInventory(new RewardsListInventory(playerdata).getInventory());
 				});
 				
-				setItem(6, Material.BOOKSHELF).n(ChatColor.YELLOW + "Rewards (" + numberOfRewards + ")");
+				setItem(rewards, Material.BOOKSHELF).n(ChatColor.YELLOW + "Rewards (" + numberOfRewards + ")");
 				
 			}
 			
 			
+            final int staff = 9;
 			/* 
 			 * Staff Commands
 			 * [] [] [] [] [] [] [] [] []
-			 * [] [] [] [] [] [] [] [] []
 			 * XX [] [] [] [] [] [] [] []
+			 * [] [] [] [] [] [] [] [] []
 			 */
 			if (role.compareTo(PGFRole.STAFF) <= 0)
 			{
-				setAction(18, (player, event) -> {
+				setAction(staff, (player, event) -> {
 					player.openInventory(new StaffInventory(playerdata).getInventory());
 					
 				});
 				
-				setItem(18, Material.SPYGLASS).n(ChatColor.LIGHT_PURPLE + "Staff Commands");
+				setItem(staff, Material.SPYGLASS).n(ChatColor.LIGHT_PURPLE + "Staff Commands");
 				
 			}
 			
 			
 			
 			
+            final int particles = 21;
 			/* 
 			 * Particle Effects
-			 * [] [] [] [] XX [] [] [] []
 			 * [] [] [] [] [] [] [] [] []
 			 * [] [] [] [] [] [] [] [] []
+			 * [] [] [] XX [] [] [] [] []
 			 */
 			if (role.compareTo(PGFRole.VETERAN) <= 0 || playerdata.hasPermission("net.pgfmc.survival.particleeffects")) // has permission
 			{
-				setAction(4, (player, event) -> {
+				setAction(particles, (player, event) -> {
 					final String particle_effect = playerdata.getData("particle_effect");
 					
 					if (particle_effect == null)
@@ -364,17 +363,17 @@ public class CommandMenuInventory implements InventoryHolder {
 				
 				if (particle == null)
 				{
-					setItem(4, Material.GLASS_BOTTLE).n(ChatColor.DARK_RED + "Particle Effects").l(Arrays.asList(ChatColor.RED + "Off"
+					setItem(particles, Material.GLASS_BOTTLE).n(ChatColor.DARK_RED + "Particle Effects").l(Arrays.asList(ChatColor.RED + "Off"
 							, ChatColor.GRAY + "Click to cycle particle effects. Donator perk!"));
 				} else
 				{
-					setItem(4, Material.DRAGON_BREATH).n(ChatColor.DARK_RED + "Particle Effects").l(Arrays.asList(ChatColor.RED + "" + particle.name()
+					setItem(particles, Material.DRAGON_BREATH).n(ChatColor.DARK_RED + "Particle Effects").l(Arrays.asList(ChatColor.RED + "" + particle.name()
 							, ChatColor.GRAY + "Click to cycle particle effects. Donator perk!"));
 				}
 				
 			} else // No permission
 			{
-				setAction(4, (player, event) -> {
+				setAction(particles, (player, event) -> {
 					final String particle_effect = playerdata.getData("particle_effect");
 					
 					// Reset to null if they don't have permission to use the particle effects
@@ -388,22 +387,24 @@ public class CommandMenuInventory implements InventoryHolder {
 					
 				});
 				
-				setItem(4, Material.GLASS_BOTTLE).n(ChatColor.DARK_RED + "Particle Effects").l(Arrays.asList(ChatColor.RED + "Off"
+				setItem(particles, Material.GLASS_BOTTLE).n(ChatColor.DARK_RED + "Particle Effects").l(Arrays.asList(ChatColor.RED + "Off"
 						, ChatColor.GRAY + "Click to cycle particle effects. Donator perk!"));
 				
 			}
 			
+
+            final int servers = 10;
 			/* 
 			 * Soft Depends on Proxycore
 			 * 
 			 * Connect / Server Selector
 			 * [] [] [] [] [] [] [] [] []
-			 * XX [] [] [] [] [] [] [] []
+			 * [] XX [] [] [] [] [] [] []
 			 * [] [] [] [] [] [] [] [] []
 			 */
 			if (playerdata.hasPermission("net.pgfmc.proxycore.connect"))
 			{
-				setItem(9, Material.COMPASS)
+				setItem(servers, Material.COMPASS)
 					.n(ChatColor.GOLD + "Server Selector")
 					.l(ChatColor.GRAY + "Connect to another server on the network.");
 				
@@ -411,7 +412,7 @@ public class CommandMenuInventory implements InventoryHolder {
 				
 				if (proxycorePlugin != null && proxycorePlugin.isEnabled()) // Proxycore is loaded and working
 				{
-					setAction(9, (player, event) -> {
+					setAction(servers, (player, event) -> {
 						final BaseInventory serverSelector = new ServerSelectorInventory(playerdata);
 						serverSelector.setBack(0, new CommandMenuInventory(playerdata).getInventory());
 						
@@ -420,7 +421,7 @@ public class CommandMenuInventory implements InventoryHolder {
 					
 				} else // Proxycore is not loaded
 				{
-					setAction(9, (player, event) -> {
+					setAction(servers, (player, event) -> {
 						player.sendMessage(ChatColor.RED + "The server selector is not available right now.");
 						playerdata.playSound(Sound.ENTITY_VILLAGER_NO);
 					});
