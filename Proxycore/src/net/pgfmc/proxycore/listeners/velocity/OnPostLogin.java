2158d56efc38f1d3294f675d64ecaa0080607b1b
bk <33919755+bkiscool@users.noreply.github.com>
2024-06-12 22:57:48 -0500

    tablist thing aaaaa

--- Proxycore/src/net/pgfmc/proxycore/listeners/velocity/OnPostLogin.java
+++ Proxycore/src/net/pgfmc/proxycore/listeners/velocity/OnPostLogin.java
@@ -1,6 +1,8 @@
 package net.pgfmc.proxycore.listeners.velocity;
 
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.PostLoginEvent;
@@ -9,6 +11,7 @@ import com.velocitypowered.api.proxy.Player;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import net.pgfmc.proxycore.Main;
 import net.pgfmc.proxycore.bot.util.MessageHandler;
 import net.pgfmc.proxycore.util.GlobalPlayerData;
 import net.pgfmc.proxycore.util.roles.RoleManager;
@@ -18,13 +21,11 @@ public class OnPostLogin {
 	@Subscribe
 	public void onJoin(PostLoginEvent e)
 	{
-		/*
 		new CompletableFuture<Void>()
 		.completeOnTimeout(null, 500L, TimeUnit.MILLISECONDS)
 		.whenComplete((nullptr, exception) -> {
 			Main.plugin.updateTablist();
 		});
-		*/
 		
 		final Player player = e.getPlayer();
 		final UUID uuid = player.getUniqueId();
@@ -49,10 +50,6 @@ public class OnPostLogin {
 		{
 			GlobalPlayerData.setData(uuid, "username", player.getUsername());
 		}
-		
-		final String discordUserId = RoleManager.getDiscordUserIdFromPlayerUuid(uuid);
-		
-		if (discordUserId == null || discordUserId.isBlank()) return;
 		
 		RoleManager.updatePlayerRole(uuid);
 		
