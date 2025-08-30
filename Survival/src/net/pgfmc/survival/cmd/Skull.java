40d38c27c811a27c6aeeace995cc7f697537ba7e
bk <33919755+bkiscool@users.noreply.github.com>
2025-08-29 22:56:44 -0500

    Fixes #252

--- Survival/src/net/pgfmc/survival/cmd/Skull.java
+++ Survival/src/net/pgfmc/survival/cmd/Skull.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
@@ -27,15 +28,19 @@ public class Skull extends ServerPlayerAtPlayerCommand {
 		return true;
 	}
 	
-	public static ItemStack getHead(UUID player)
+	public static ItemStack getHead(UUID uuid)
 	{
 		// Credit: https://www.spigotmc.org/threads/skullmeta-with-custom-skin-in-gui-1-14-4.403014/#post-3598618
 		// Edited by me lol
-		
+
+		OfflinePlayer player = Bukkit.getOfflinePlayer(uuid);
 		ItemStack item = new ItemStack(Material.PLAYER_HEAD); // Create a new ItemStack of the Player Head type.
+
+		// Getting the skin of offline players is rate limited by Mojang servers to one call per minute
+		if (!player.isOnline()) return item; // Online players do not require an API call
+
 		SkullMeta meta = (SkullMeta) item.getItemMeta(); // Get the created item's ItemMeta and cast it to SkullMeta so we can access the skull properties
-		meta.setOwningPlayer(Bukkit.getOfflinePlayer(player)); // Set the skull's owner so it will adapt the skin of the provided username (case sensitive).
-		
+		meta.setOwningPlayer(player); // Set the skull's owner so it will adapt the skin of the provided username (case sensitive).
 		item.setItemMeta(meta); // Apply the modified meta to the initial created item
 		
 		return item;
