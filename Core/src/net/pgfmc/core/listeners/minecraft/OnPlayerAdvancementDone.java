54bfe0a69b3b5351bdfe7afb42c50985e88c31ff
bkiscool <brandon@brandonkey.me>
2026-08-24 22:50:16 -0500

    Translation using GlobalTranslator and Locale.US

--- Core/src/net/pgfmc/core/listeners/minecraft/OnPlayerAdvancementDone.java
+++ Core/src/net/pgfmc/core/listeners/minecraft/OnPlayerAdvancementDone.java
@@ -1,16 +1,18 @@
 package net.pgfmc.core.listeners.minecraft;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.advancement.Advancement;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 
-import net.kyori.adventure.text.TextComponent;
-import net.kyori.adventure.translation.GlobalTranslator;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TranslatableComponent;
 import net.pgfmc.core.api.playerdata.PlayerData;
-import net.pgfmc.core.util.Logger;
-import net.pgfmc.core.util.Translation;
+import net.pgfmc.core.util.TranslatableComponentDecoder;
 import net.pgfmc.core.util.proxy.PluginMessageType;
 
 public class OnPlayerAdvancementDone implements Listener {
@@ -23,6 +25,22 @@ public class OnPlayerAdvancementDone implements Listener {
 		final Player player = e.getPlayer();
 		final PlayerData playerdata = PlayerData.from(player);
 		final Advancement advancement = e.getAdvancement();
+		final Component advancementMessageComponent = e.message();
+		
+		if (advancementMessageComponent instanceof TranslatableComponent advancementMessageTranslatableComponent) {
+			if (!advancementMessageTranslatableComponent.arguments().isEmpty()) {
+				final List<Component> newArgs = new ArrayList<>();
+				newArgs.add(playerdata.getRankedName());
+				
+				advancementMessageTranslatableComponent.arguments(newArgs);
+				e.message(advancementMessageTranslatableComponent);
+			}
+			
+			final String plaintext = TranslatableComponentDecoder.translate(advancementMessageTranslatableComponent);
+			PluginMessageType.DISCORD_MESSAGE.send(player, "<:dwarf:1191762269261017119> " + playerdata.getDisplayName() + " " + plaintext);
+		}
+		
+		
 
         //String advancementComponent = ((TextComponent) advancement.getDisplay().title()).content();
         //Logger.log(advancement.displayName().toString());
@@ -30,7 +48,7 @@ public class OnPlayerAdvancementDone implements Listener {
 
 
 
-
+		/*
         TextComponent translated = (TextComponent) GlobalTranslator.render(advancement.getDisplay().title(), Translation.DISCORD_LOCALE);
 
 
@@ -52,6 +70,7 @@ public class OnPlayerAdvancementDone implements Listener {
 				advancementMessage = "has made the advancement [" + advancement.getDisplay().title() + "]!";
 				break;
 		}
+		*/
 		
 		/*
 		 * No easy way to cancel/set the advancement message
@@ -69,6 +88,6 @@ public class OnPlayerAdvancementDone implements Listener {
 		*/
 		
 		//PluginMessageType.DISCORD_MESSAGE.send(player, "<:dwarf:1191762269261017119> " + playerdata.getDisplayName() + " " + translated);
-		PluginMessageType.DISCORD_MESSAGE.send(player, "<:dwarf:1191762269261017119> " + playerdata.getDisplayName() + advancementMessage);
+		//PluginMessageType.DISCORD_MESSAGE.send(player, "<:dwarf:1191762269261017119> " + playerdata.getDisplayName() + advancementMessage);
 	}
 }
--- Core/src/net/pgfmc/core/listeners/minecraft/OnPlayerDeath.java
+++ Core/src/net/pgfmc/core/listeners/minecraft/OnPlayerDeath.java
@@ -1,12 +1,22 @@
 package net.pgfmc.core.listeners.minecraft;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.TranslatableComponent;
+import net.kyori.adventure.text.TranslationArgument;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import net.pgfmc.core.CoreMain;
 import net.pgfmc.core.api.playerdata.PlayerData;
+import net.pgfmc.core.util.TranslatableComponentDecoder;
 import net.pgfmc.core.util.proxy.PluginMessageType;
 
 public class OnPlayerDeath implements Listener {
@@ -16,6 +26,39 @@ public class OnPlayerDeath implements Listener {
 	{
 		final Player player = e.getEntity();
 		final PlayerData playerdata = PlayerData.from(player);
+		final Component deathMessage = e.deathMessage();
+		
+		if (deathMessage instanceof TranslatableComponent deathMessageTransComponent) {
+			final List<TranslationArgument> args = deathMessageTransComponent.arguments();
+			final List<Component> newArgs = new ArrayList<>();
+			
+			final Entity causingEntity = e.getDamageSource().getCausingEntity();
+	        PlayerData causingPlayer = null;
+
+	        if (causingEntity instanceof Player) {
+	            final Player causingEntityPlayer = (Player) causingEntity;
+	            causingPlayer = PlayerData.from(causingEntityPlayer);
+	        }
+			
+			for (int i = 0; i < args.size(); i++) {
+	            final String deadPlayer = PlainTextComponentSerializer.plainText().serialize(args.get(i).asComponent());
+	            
+	            if (deadPlayer.equals(player.getName())) {
+	            	newArgs.add(i, playerdata.getRankedName());
+                } else if (causingPlayer != null && deadPlayer.equals(causingPlayer.getDisplayName())) {
+                    newArgs.add(i, causingPlayer.getRankedName());
+                } else {
+                    newArgs.add(Component.text(deadPlayer));
+                }
+	        }
+			
+			deathMessageTransComponent.arguments(newArgs);
+			e.deathMessage(deathMessageTransComponent);
+			
+			final String translated = TranslatableComponentDecoder.translate(deathMessageTransComponent);
+			PluginMessageType.DISCORD_MESSAGE.send(playerdata.getPlayer(), "<:DEATH:907865162558636072> " + translated);
+			PluginMessageType.MESSAGE.send(player, deathMessageTransComponent);
+		}
 
 
 
@@ -65,13 +108,13 @@ public class OnPlayerDeath implements Listener {
         //Logger.log(output);
 
 
-        String message = playerdata.getDisplayName() + " has died. (" + CoreMain.plugin.serverDisplayName() + ")";
+        //String message = playerdata.getDisplayName() + " has died. (" + CoreMain.plugin.serverDisplayName() + ")";
 
 
 
 
 //		PluginMessageType.MESSAGE.send(player, deathMessage);
-		PluginMessageType.DISCORD_MESSAGE.send(playerdata.getPlayer(), "<:DEATH:907865162558636072> " + message);
+		//PluginMessageType.DISCORD_MESSAGE.send(playerdata.getPlayer(), "<:DEATH:907865162558636072> " + message);
 		e.deathMessage(null);
 	}
 }
